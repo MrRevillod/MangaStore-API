d2306f07d74da5da98c61362054ba9d049c9fffc
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-07 21:34:08 -0300

    lock

--- src/config.js
+++ src/config.js
@@ -5,6 +5,6 @@ config();
 export const PORT = process.env.API_PORT || 3000;
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 28081911;
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root28081911';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const DB_DATABASE = process.env.DB_DATABASE || 'MangaStore';
