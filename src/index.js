2d5f643c83ab759e14bf1f3231dba0e1c5c3f388
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-07 22:52:11 -0300

    no-cors

--- src/index.js
+++ src/index.js
@@ -11,10 +11,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+app.use(cors());
 
 app.use('/api', apiRoutes);
 
