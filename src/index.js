541af89902c43bae6ae3aa644e98e78abe633c4b
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-07 21:36:44 -0300

    PORT

--- src/index.js
+++ src/index.js
@@ -19,6 +19,5 @@ app.use((req, res, next) => {
     res.status(404).json({ message: "Not found" })
 });
 
-app.listen(PORT, () => {
-    console.log("ExpressJS server APi started");
-});
+app.listen(PORT)
+console.log(`Server is running on port ${PORT}`);
