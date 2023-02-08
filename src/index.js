811a3fa7313ca2d0fdecb1202c235d2c18c97781
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-07 22:56:10 -0300

    q pasa cors

--- src/index.js
+++ src/index.js
@@ -7,11 +7,12 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(cors());
 
 app.use('/api', apiRoutes);
 
