00d4330c54f2a054ee5451ac439c003b0b1177a6
MrRevillod <lrevillod2022@alu.uct.cl>
2023-02-07 21:15:50 -0300

    route

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 import { createPool } from 'mysql2/promise';
 
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE } from './src/config.js';
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_PORT, DB_DATABASE } from './config.js';
 
 export const Pool = createPool({
     host: DB_HOST,
